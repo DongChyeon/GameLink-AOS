b7a99cbcae70b633abce63c80d9136777aa35d58
dongchyeon <kimdonghyun90766@gmail.com>
2024-09-24 12:22:05 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GameLink"
+include(":app")
+include(":data")
+include(":core")
+include(":core:domain")
+include(":core:designsytem")
+include(":core:common")
+include(":feature:auth")
